a82fa91f7400f9eb69058a5bb52d3e4825a504ad
daxdax89 <daxdax89@msn.com>
2017-02-02 09:24:33 +0100

    Napravljene nase klase

--- src/main/java/rs/co/micro/micro/BwTable.java
+++ src/main/java/rs/co/micro/micro/BwTable.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package rs.co.micro.micro;
 
 import com.vaadin.data.util.sqlcontainer.SQLContainer;
 import com.vaadin.data.util.sqlcontainer.connection.SimpleJDBCConnectionPool;
 import com.vaadin.data.util.sqlcontainer.query.FreeformQuery;
+import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.Table;
 import java.sql.SQLException;
 import java.util.Locale;
@@ -16,12 +12,11 @@ import java.util.Locale;
  *
  * @author damir
  */
-public class BwTable extends Table {
+public class BwTable extends CustomComponent{
 
     public BwTable() throws SQLException{
 
-        Table podaci2 = new Table();
-        podaci2.addContainerProperty("Sifra partnera", String.class, null);
+        Table podaci = new Table();
         try {
             SimpleJDBCConnectionPool connectionPool = new SimpleJDBCConnectionPool("org.postgresql.Driver", "jdbc:postgresql://10.1.2.3:5432/BW6", "postgres", "superset");
             SQLContainer container = new SQLContainer(new FreeformQuery(
@@ -36,8 +31,8 @@ public class BwTable extends Table {
                     + "LEFT OUTER JOIN (SELECT kontobroj, kontonaziv[1] AS kontonaziv, kontopartner, kontoprenos, kontodevizni FROM BUSINESS.kplan_1) t2 on t2.kontobroj = t1.finkonto \n"
                     + "LEFT OUTER JOIN (SELECT partsifra, partfirma, partpreduzece, partmesto, partposta, partadresa, partmaticnibroj, partdefracun, partdeftelefon, CASE WHEN LENGTH(TRIM(partpib)) = 0 THEN partmaticnibroj ELSE partpib END AS partpib, parttip, partpdvstatus, partstatus, partkategorija FROM BUSINESS.partneri) t3 ON t1.FinPartner = t3.partsifra\n"
                     + "WHERE  finnalog <> '1' AND  FinKonto = '2040' AND FinPartner = 1058  AND FinGodina >= 2016 AND FinDatNal BETWEEN '2016-01-01' AND '2017-01-11')", connectionPool));
-            podaci2 = new Table("", container);
-            podaci2.setSortEnabled(true);
+            podaci = new Table("", container);
+            podaci.setSortEnabled(true);
             System.out.println(container);
             System.out.println("radi");
 
@@ -46,56 +41,54 @@ public class BwTable extends Table {
         }
 
         //Dodeljivanje imena kolona
-        podaci2.setColumnHeader("finkonto", "Šifra konta");
-        podaci2.setColumnHeader("finpartner", "Šifra partnera");
-        podaci2.setColumnHeader("finnalog", "Šifra partnera");
-        podaci2.setColumnHeader("findatnal", "Datum naloga");
-        podaci2.setColumnHeader("finvezni", "Šifra partnera");
-        podaci2.setColumnHeader("findokument", "Šifra dokumenta");
-        podaci2.setColumnHeader("findatdok", "Šifra partnera");
-        podaci2.setColumnHeader("partpreduzece", "Šifra partnera");
-        podaci2.setColumnHeader("partmesto", "Mesto partnera");
-        podaci2.setColumnHeader("partadresa", "Adresa partnera");
-        podaci2.setColumnHeader("partdeftelefon", "Telefon partnera");
-        podaci2.setColumnHeader("partdefracun", "Račun partnera");
-        podaci2.setColumnHeader("kontonaziv", "Konto naziv");
-        podaci2.setColumnHeader("kontopartner", "Konto partnera");
-        podaci2.setColumnHeader("finduguje", "Dugovanje partnera");
-        podaci2.setColumnHeader("finpotrazuje", "Potraživanje partnera");
-        podaci2.setColumnHeader("finnalog", "Šifra partnera");
-        podaci2.setColumnHeader("finnalog", "Šifra partnera");
-        podaci2.setColumnHeader("finnalog", "Šifra partnera");
-        podaci2.setColumnHeader("finnalog", "Šifra partnera");
-        podaci2.setColumnHeader("finnalog", "Šifra partnera");
-        podaci2.setColumnHeader("finnalog", "Šifra partnera");
-        podaci2.setColumnHeader("finnalog", "Šifra partnera");
-        podaci2.setColumnHeader("finnalog", "Šifra partnera");
-        podaci2.setColumnHeader("finnalog", "Šifra partnera");
-        podaci2.setColumnHeader("finnalog", "Šifra partnera");
-        podaci2.setColumnHeader("finnalog", "Šifra partnera");
-        podaci2.setColumnHeader("finnalog", "Šifra partnera");
-        podaci2.setColumnHeader("finnalog", "Šifra partnera");
-        podaci2.setColumnHeader("finnalog", "Šifra partnera");
-        podaci2.setColumnHeader("finnalog", "Šifra partnera");
-        podaci2.setColumnHeader("finnalog", "Šifra partnera");
+        podaci.setColumnHeader("finkonto", "Šifra konta");
+        podaci.setColumnHeader("finpartner", "Šifra partnera");
+        podaci.setColumnHeader("finnalog", "Šifra partnera");
+        podaci.setColumnHeader("findatnal", "Datum naloga");
+        podaci.setColumnHeader("finvezni", "Šifra partnera");
+        podaci.setColumnHeader("findokument", "Šifra dokumenta");
+        podaci.setColumnHeader("findatdok", "Šifra partnera");
+        podaci.setColumnHeader("partpreduzece", "Šifra partnera");
+        podaci.setColumnHeader("partmesto", "Mesto partnera");
+        podaci.setColumnHeader("partadresa", "Adresa partnera");
+        podaci.setColumnHeader("partdeftelefon", "Telefon partnera");
+        podaci.setColumnHeader("partdefracun", "Račun partnera");
+        podaci.setColumnHeader("kontonaziv", "Konto naziv");
+        podaci.setColumnHeader("kontopartner", "Konto partnera");
+        podaci.setColumnHeader("finduguje", "Dugovanje partnera");
+        podaci.setColumnHeader("finpotrazuje", "Potraživanje partnera");
+        podaci.setColumnHeader("finnalog", "Šifra partnera");
+        podaci.setColumnHeader("finnalog", "Šifra partnera");
+        podaci.setColumnHeader("finnalog", "Šifra partnera");
+        podaci.setColumnHeader("finnalog", "Šifra partnera");
+        podaci.setColumnHeader("finnalog", "Šifra partnera");
+        podaci.setColumnHeader("finnalog", "Šifra partnera");
+        podaci.setColumnHeader("finnalog", "Šifra partnera");
+        podaci.setColumnHeader("finnalog", "Šifra partnera");
+        podaci.setColumnHeader("finnalog", "Šifra partnera");
+        podaci.setColumnHeader("finnalog", "Šifra partnera");
+        podaci.setColumnHeader("finnalog", "Šifra partnera");
+        podaci.setColumnHeader("finnalog", "Šifra partnera");
+        podaci.setColumnHeader("finnalog", "Šifra partnera");
+        podaci.setColumnHeader("finnalog", "Šifra partnera");
+        podaci.setColumnHeader("finnalog", "Šifra partnera");
+        podaci.setColumnHeader("finnalog", "Šifra partnera");
 
-//        podaci.addContainerProperty("Šifra partnera", String.class, null);
-//podaci.addContainerProperty("Godine", Integer.class, null);
-//podaci.addContainerProperty("Grad", String.class, null);
 //// Show exactly the currently contained rows (items)
-//        podaci.setPageLength(podaci.size());
-        podaci2.setId("tabela2");
-        podaci2.setColumnAlignments(new Table.Align[]{Table.Align.LEFT, Table.Align.RIGHT, Table.Align.LEFT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT,
+//podaci.setPageLength(podaci.size());
+        podaci.setId("tabela");
+        podaci.setColumnAlignments(new Table.Align[]{Table.Align.LEFT, Table.Align.RIGHT, Table.Align.LEFT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT,
             Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT,
             Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT,
             Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT, Table.Align.RIGHT});
-        podaci2.setSizeFull();
-        podaci2.addStyleName("example-table2");
-        podaci2.setLocale(Locale.ITALY);
-        podaci2.setSortEnabled(true);
-        podaci2.setWidth("98%");
-        podaci2.setImmediate(true);
+        podaci.setSizeFull();
+        podaci.addStyleName("stilTabele");
+        podaci.setLocale(Locale.ITALY);
+        podaci.setSortEnabled(true);
+        podaci.setWidth("98%");
+        podaci.setImmediate(true);
         System.out.println("Ubacena nasa tabela");
+        setCompositionRoot(podaci);
         
         
     }
